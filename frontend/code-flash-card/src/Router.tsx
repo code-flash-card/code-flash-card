b84682091bc323a4c8f843f36aac7083f3e045d4
Yang Hong Seok <davidyang2149@gmail.com>
2022-08-27 23:20:07 +0900

    :hammer: 카드 상세 페이지 라우트를 설정합니다

--- frontend/code-flash-card/src/Router.tsx
+++ frontend/code-flash-card/src/Router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import MainPage from "./main/index";
 import MakeCard from "./main/MakeCard";
+import CardDetailPage from "./pages/CardDetailPage";
 
 function Router() {
     return (
     <BrowserRouter>
-         <Routes>  
+        <Routes>  
             <Route path="/" element={<MainPage />} />
             <Route path="/makecard" element={<MakeCard />} />
+            <Route path="/detail/:cardId" element={<CardDetailPage />} />
         </Routes>
     </BrowserRouter>
     );
