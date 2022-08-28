7c5570702bb07bbc059ea4f8512dd93512004c2a
DESKTOP-1E9IN30\82104 <tpoluol3@gmail.com>
2022-08-28 16:50:16 +0900

    카드 리스트중 한개의 카드를 클릭했을때 카드 상세페이지를 보여주는 작업

--- frontend/code-flash-card/src/components/FlashCard.tsx
+++ frontend/code-flash-card/src/components/FlashCard.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 import { Card } from "./FlashCards";
 
 export default function FlashCard({ card }: { card: Card }) {
-  const { explain, viewCount } = card;
-  return (
-    <li>
-      <p>{explain}</p>
-      <span>{viewCount}</span>
-    </li>
-  );
+    const { explain, viewCount, cardId } = card;
+    return (
+        <li>
+            <Link to={`/detail/${cardId}`}>
+                <p>{explain}</p>
+                <span>{viewCount}</span>
+            </Link>
+        </li>
+    );
 }
