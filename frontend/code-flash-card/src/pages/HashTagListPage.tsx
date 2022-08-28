66bf6a9ace0556926b53b154ab2cd0b3dbf7143b
Yang Hong Seok <davidyang2149@gmail.com>
2022-08-28 15:34:14 +0900

    :hammer: 콘솔 로그를 제거합니다

--- frontend/code-flash-card/src/pages/HashTagListPage.tsx
+++ frontend/code-flash-card/src/pages/HashTagListPage.tsx
@@ -10,7 +10,7 @@ const url = `https://weareboard.kr/teosp/v1/card`;
 
 export default function HashTagListPage() {
     const { data, error } = useFetch<Card[]>(url);
-    // console.log(data);
+    
     const cards =
         data?.map((card) => {
             return {
