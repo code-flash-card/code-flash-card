24071f2b964183485a916f53465d0f526431de2a
DESKTOP-1E9IN30\82104 <tpoluol3@gmail.com>
2022-08-29 17:12:11 +0900

    해쉬태그 리스트 페이지, 카드 상세 페이지 뒤로가기 버튼 기능 구현

--- frontend/code-flash-card/src/components/BackSpaceBtn.tsx
+++ frontend/code-flash-card/src/components/BackSpaceBtn.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import images from "../assets/images";
 
 export default function BackSpaceBtn() {
+  const navigate = useNavigate()
+  const onClickBack = () => {
+    navigate(`/`)
+  }
     return (
-        <img src={images.icon_back} style={{ width: '24px', height: '24px' }} />
+        <img src={images.icon_back} style={{ width: '24px', height: '24px' }} onClick={onClickBack}/>
     );
 }
--- frontend/code-flash-card/src/components/CloseBtn.tsx
+++ frontend/code-flash-card/src/components/CloseBtn.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import images from "../assets/images";
 
 export default function CloseBtn() {
+  const navigate = useNavigate()
+  const onClickBack = () => {
+    navigate(`/`)
+  }
     return (
-        <img src={images.icon_close} />
+        <img src={images.icon_close} onClick={onClickBack}/>
     );
 }
