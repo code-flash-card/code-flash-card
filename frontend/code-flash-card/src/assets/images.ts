2fcdee733d4d72cd6b48b7450c041046497dca7a
DESKTOP-1E9IN30\82104 <tpoluol3@gmail.com>
2022-08-29 22:57:03 +0900

    이미지 경로 수정

--- frontend/code-flash-card/src/assets/images.ts
+++ frontend/code-flash-card/src/assets/images.ts
@@ -1,15 +1,14 @@
 const images = {
-    checked: "/assets/images/checked.svg",
-    logo_flip: "/assets/images/logo_flip.svg",
-    icon_github: "/assets/images/icon_github.svg",
-    icon_view: "/assets/images/icon_view.svg",
-    icon_create: "/assets/images/icon_create.svg",
-    bulb_off: "/assets/images/bulb_off.svg",
-    bulb_on: "/assets/images/bulb_on.svg",
-    icon_close: "/assets/images/icon_close.svg",
-    icon_back: "/assets/images/icon_arrow_left.svg",
-    banner: "/assets/images/banner.svg",
-    congratuation: "/assets/images/congratuation.svg",
+    checked: "/flashcard/assets/images/checked.svg",
+    logo_flip: "/flashcard/assets/images/logo_flip.svg",
+    icon_github: "/flashcard/assets/images/icon_github.svg",
+    icon_view: "/flashcard/assets/images/icon_view.svg",
+    icon_create: "/flashcard/assets/images/icon_create.svg",
+    bulb_off: "/flashcard/assets/images/bulb_off.svg",
+    icon_close: "/flashcard/assets/images/icon_close.svg",
+    icon_back: "/flashcard/assets/images/icon_arrow_left.svg",
+    banner: "/flashcard/assets/images/banner.svg",
+    congratuation: "/flashcard/assets/images/congratuation.svg",
 };
 
 export default images;
